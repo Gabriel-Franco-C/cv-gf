4b436659fc41bca35e6dd19b0c1a4e6809a5de0c
Gabriel Franco <gfranco@keiron.cl>
2023-07-02 16:22:16 -0300

    feat: :beers: Second deploy test commit, test deploy from master branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <p>Pagina principal de cv-gf</p>
       <p>Cambio de prueba para probar el deploy</p>
+      <p>Segundo ambio de prueba para probar el deploy desde la rama master</p>
     </div>
   );
 }
