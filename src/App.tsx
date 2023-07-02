583d144cac44f07101f8b86b9468253da7612740
Gabriel Franco <gfranco@keiron.cl>
2023-07-02 17:06:23 -0300

    feat: :sparkles: Add navigation bar to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import MainBackground from "./Components/mainBackground";
+
 function App() {
-  return (
-    <div className="App">
-      <p>Pagina principal de cv-gf</p>
-      <p>Cambio de prueba para probar el deploy</p>
-      <p>Segundo ambio de prueba para probar el deploy desde la rama master</p>
-    </div>
-  );
+  return <MainBackground />;
 }
 
 export default App;
